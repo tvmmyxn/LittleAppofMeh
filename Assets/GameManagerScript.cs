175e4a2e5c2caf3946b8728a10d62447e50f6e16
tvmmyxn <78470284+tvmmyxn@users.noreply.github.com>
2021-02-25 02:01:07 -0800

    Initial commit

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public string fileName = "default.txt";
+    public Text displayName;
+
+    private string[] StudentNames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStudentNames();
+        ButtonClick();
+    }
+
+    private void LoadStudentNames()
+    {
+        this.StudentNames = File.ReadAllLines(fileName);
+    }
+
+    public void ButtonClick()
+    {
+        int i = UnityEngine.Random.Range(0, this.StudentNames.Length);
+        string name = this.StudentNames[i];
+        displayName.text = name;
+    }
+
+
+}
